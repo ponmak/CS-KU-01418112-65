d3de796910f9e2ea7b2e2d1c91068e79e99ae12e
ponmak <peeranut.4498@gmail.com>
2024-07-25 00:23:04 +0700

    lab 07PM : PB_06 complete

--- elab/lab 07 PM/PB_06.py
+++ elab/lab 07 PM/PB_06.py
@@ -1,1 +1,16 @@
-# PB_06.py
+# 06 ตรวจสอบสามเหลี่ยมมุมฉาก
+
+import math
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if c == math.sqrt(a**2 + b**2):
+    print(True)
+elif a == math.sqrt(b**2 + c**2):
+    print(True)
+elif b == math.sqrt(c**2 + a**2):
+    print(True)
+else:
+    print(False)
