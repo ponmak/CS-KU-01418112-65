4b4c8ff572b675fb9f248c81d60b7c93ed22eea2
ponmak <peeranut.4498@gmail.com>
2024-07-25 00:14:35 +0700

    lab 07: PB_09 complete

--- elab/lab 07 PM/PB_09.py
+++ elab/lab 07 PM/PB_09.py
@@ -1,1 +1,36 @@
-# PB_09.py
+# 09 Find a, b in which a*b=n and (a+b) is the lowest
+
+NUM = int(input())
+
+b = 1
+temp = 0
+
+while b < NUM:
+    a = int(NUM / b)
+    
+    if a*b == NUM and temp == 0:
+            temp = a+b
+            b += 1
+            continue
+    elif a * b == NUM and temp != 0:
+        if temp < a+b:
+            b += 1
+            continue
+        elif temp > a+b:
+            temp = a+b
+            b += 1
+            continue
+        else:
+            b += 1
+            continue
+    else:
+        b += 1
+        continue
+
+print(temp)
+
+
+
+
+
+
