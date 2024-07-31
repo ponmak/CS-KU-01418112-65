e0ded86e66c608758206f830388161fa1f314a14
ponmak <peeranut.4498@gmail.com>
2024-07-31 22:33:56 +0700

    lab 05PB: 02 update (doing now)

--- elab/lab 05 PB/PB_02.py
+++ elab/lab 05 PB/PB_02.py
@@ -7,3 +7,8 @@ buyAmt = int(input('Enter shopping amount: '))
 
 all_minutes = (hours * 60) + minutes
 round_up_hour = math.ceil(all_minutes / 60)
+
+if hours < 0 or hours > 20 or minutes < 0 or minutes > 59:
+    print('Invalid time.')
+else:
+    
