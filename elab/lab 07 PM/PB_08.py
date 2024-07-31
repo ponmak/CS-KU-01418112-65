6ee61d4313068773667844b7f2890f7f444477d7
ponmak <peeranut.4498@gmail.com>
2024-07-31 21:29:50 +0700

    lab 07 08

--- elab/lab 07 PM/PB_08.py
+++ elab/lab 07 PM/PB_08.py
@@ -1,1 +1,27 @@
 # PB_08.py
+day_index = input() 
+day_in_months = int(input())
+
+MODUL_DAY = 0
+
+if day_index == 'Sunday':
+    day_index = 1
+elif day_index == 'Monday':
+    day_index = 2
+elif day_index == 'Tuesday':
+    day_index = 3
+elif day_index == 'Wednesday':
+    day_index = 4
+elif day_index == 'Thursday':
+    day_index = 5
+elif day_index == 'Friday':
+    day_index = 6
+elif day_index == 'Saturday':
+    day_index  = 7
+
+# Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+if day_index > 7 or day_index < 1 or day_in_months > 32:
+    print('ERROR')
+else:
+    MODUL_DAY = day_in_months % 7 
+    print(MODUL_DAY)
