52dac8cabd14704a69f420c8662432cedd654581
ponmak <peeranut.4498@gmail.com>
2024-08-02 22:22:02 +0700

    lab 05 complete

--- elab/lab 05 PB/PB_02.py
+++ elab/lab 05 PB/PB_02.py
@@ -8,7 +8,28 @@ buyAmt = int(input('Enter shopping amount: '))
 all_minutes = (hours * 60) + minutes
 round_up_hour = math.ceil(all_minutes / 60)
 
+total = 0
+
 if hours < 0 or hours > 20 or minutes < 0 or minutes > 59 or (hours == 20 and minutes > 0):
     print('Invalid time.')
 else:
+    if 1 <= round_up_hour < 3 or buyAmt > 3000:
+        print('No charge, thank you.')
+        pass
+    elif 3 <= round_up_hour <= 4 and not 300 <= buyAmt <= 3000:
+        total = 20 * (round_up_hour - 2)
+        print(f'Total amount due is {total} Baht, thank you.')
+    elif 3 <= round_up_hour <= 4 and  300 <= buyAmt <= 3000:
+        print('No charge, thank you.')
+        pass
+    elif round_up_hour > 5 and not buyAmt > 3000 and not 300 <= buyAmt <= 3000:
+        total = 40 + 50 * (round_up_hour - 4)
+        print(f'Total amount due is {total} Baht, thank you.')
+    else:
+        total = 50 * (round_up_hour - 4)
+        print(f'Total amount due is {total} Baht, thank you.')
+
+
+
+    
     
--- elab/lab 06 PB/PB_01.py
+++ elab/lab 06 PB/PB_01.py
@@ -1,1 +1,39 @@
-# 01
+# 01 Master Mind
+
+TARGET = int(input('Enter a target (4-digit integer): '))
+GUESS = int(input('Enter your guess (4-digit integer): '))
+
+target_range_counter = 0
+guess_range_counter = 0
+
+# correct
+digits = 0
+position = 0
+
+target_temp = TARGET
+guess_temp = GUESS
+
+if TARGET == GUESS:
+    print('Congratulations, you just mastered my mind!!')
+else:
+    while target_range_counter < 4:
+        mod_tar = target_temp % 10
+        target_temp //= 10
+        guess_range_counter = 0
+        while guess_range_counter < 4:
+            mod_guess = guess_temp % 10
+            guess_temp //= 10
+
+            if mod_tar == mod_guess and target_range_counter == guess_range_counter:
+                digits += 1
+                position +=1
+                pass
+            elif mod_tar == mod_guess and target_range_counter != guess_range_counter:
+                digits += 1
+                pass
+            else:
+                pass
+            target_range_counter +=1
+            guess_range_counter += 1
+
+print(position,digits)
