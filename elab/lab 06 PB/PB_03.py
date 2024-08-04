d38e8e071a7b46030790640f9b38e2479f4d7841
ponmak <peeranut.4498@gmail.com>
2024-08-05 00:19:51 +0700

    lab 07 : 03 done

--- elab/lab 06 PB/PB_03.py
+++ elab/lab 06 PB/PB_03.py
@@ -1,1 +1,24 @@
-# 03
+# 03 Guessing 3
+
+counter = 0
+target  = 72
+
+while True:
+    
+    NUM_GUSESS = int(input('Enter your guess: '))
+    
+    if NUM_GUSESS < 0 or NUM_GUSESS > 100 : 
+        print('Sorry, your guess is out of range.')
+        counter += 1
+    elif NUM_GUSESS == target :
+        print('Congratulations, your guess is correct.')
+        counter += 1
+        break
+    elif NUM_GUSESS < target :
+        print('Sorry, your guess is too low.')
+        counter += 1
+    else :
+        print('Sorry, your guess is too high.')
+        counter += 1
+
+print(f'Total number of guesses is {counter}.')
