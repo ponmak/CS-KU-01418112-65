a8851bb5beb57ccabe0646bda96291e1e6d9629e
ponmak <peeranut.4498@gmail.com>
2024-08-20 00:00:08 +0700

    Lab 07PM: 08PM (incomplete)

--- elab/lab 07 PM/PB_08.py
+++ elab/lab 07 PM/PB_08.py
@@ -1,1 +1,115 @@
 # 08 วันของเดือนมกราคม 2
+
+day_start = input() 
+day_index = int(input())
+
+if  0 < day_index < 32:
+    if day_start == 'Sunday':
+        if (day_index - 1) % 7 == 0:
+            print()
+        elif (day_index - 1) % 7 == 1:
+            print()
+        elif (day_index - 1) % 7 == 2:
+            print()
+        elif (day_index - 1) % 7 == 3:
+            pt
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Monday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Tuesday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Wednesday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Thursday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Friday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    elif day_start == 'Saturday':
+        if (day_index-1) % 7 == 0:
+            
+        elif (day_index - 1) % 7 == 1:
+
+        elif (day_index - 1) % 7 == 2:
+
+        elif (day_index - 1) % 7 == 3:
+
+        elif (day_index - 1) % 7 == 4:
+
+        elif (day_index - 1) % 7 == 5:
+
+        elif (day_index - 1) % 7 == 6:
+            
+    else:
+        print("ERROR")
+else:
+    print("ERROR")
