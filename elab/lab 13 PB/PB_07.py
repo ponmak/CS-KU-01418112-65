b958bd69960333d0e7f980f89f1b07048466eb91
ponmak <peeranut.4498@gmail.com>
2024-10-07 22:32:15 +0700

    pb07 update

--- elab/lab 13 PB/PB_07.py
+++ elab/lab 13 PB/PB_07.py
@@ -6,6 +6,12 @@ m, n = int(m), int(n)
 # setup board
 matrix = [[0 for j in range(m)] for i in range(n)]
 
+# planting
+bomb_count = int(input())
+for i in range(bomb_count):
+    x, y = input().split(',')
+    x, y = int(x), int(y)
+    matrix[x][y] = '*'
 
-
+print(matrix)
 
