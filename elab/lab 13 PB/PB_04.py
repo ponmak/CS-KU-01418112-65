559950971dbdece5f3e82cf42ddf7fcf6992a431
ponmak <peeranut.4498@gmail.com>
2024-10-15 23:28:52 +0700

    lab 13PB update

--- elab/lab 13 PB/PB_04.py
+++ elab/lab 13 PB/PB_04.py
@@ -1,2 +1,60 @@
-# 04 จำนวนวันอาทิตย์ 
+# 04 จำนวนวันอาทิตย์
+import math
 
+def check_valid_date(day, month):
+    day_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if day < 1 or day > day_in_month[month]:
+        return False
+    return True
+
+def check_valid_month(month):
+    if month < 1 or month > 12:
+        return False
+    return True
+
+def count_sunday_in_range(start, end, first_sunday):
+    DAYINMONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if start == end and (start[0] - first_sunday) % 7 == 0:
+
+        return 1
+
+    count = 0 + end[0] - start[0] + 1
+    if start[1] != end[1]:
+        count += DAYINMONTH[start[1] - 1] - start[0] + 1
+        count += end[0]
+        for i in range(start[1], end[1] - 1):
+            count += DAYINMONTH[i]
+    else:
+        count = abs(end[0] - start[0]) + 1
+
+    first_sunday_date = first_sunday
+    sundays = 0
+
+    while first_sunday_date < start[0]:
+        first_sunday_date += 7
+
+    while first_sunday_date <= end[0]:
+        sundays += 1
+        first_sunday_date += 7
+
+    return sundays
+
+# รับข้อมูล
+start = input().split('-')
+start = [int(i) for i in start]
+end = input().split('-')
+end = [int(i) for i in end]
+first_sunday = int(input())
+
+if check_valid_month(start[1]) and check_valid_month(end[1]):
+    if check_valid_date(start[0], start[1]) and check_valid_date(end[0], end[1]):
+        result = count_sunday_in_range(start, end, first_sunday)
+        print(result)
+    else:
+        print("Wrong Day")
+else:
+    print("Wrong Month")
+    if not (check_valid_date(start[0], start[1]) and check_valid_date(end[0], end[1])):
+        print("Wrong Day")
--- elab/lab 13 PB/PB_07.py
+++ elab/lab 13 PB/PB_07.py
@@ -10,7 +10,7 @@ m, n = input().split("x")
 m, n = int(m), int(n)
 
 # setup board
-matrix = [[0 for j in range(m)] for i in range(n)]
+matrix = [[0 for j in range(n)] for i in range(m)]
 
 # planting
 bomb_count = int(input())
@@ -18,17 +18,50 @@ for i in range(bomb_count):
     x, y = input().split(',')
     x, y = int(x), int(y)
     # change bomb to *
-    matrix[y][x] = "*" # shutup Pyrigh
+    matrix[x][y] = "*" # shutup Pyright
 
-    # check 8 directions
-    for i in range(m):
-        for j in range(n):
-            # top left
-            if i == 0 and j == 0 and matrix[i][j] == '*':
-                matrix[i][j+1] += 1
-                matrix[i+1][j] += 1
-                matrix[i+1][j+1] += 1
-            # 
+# check 8 directions
+for i in range(m):
+    for j in range(n):
+        if matrix[i][j] == '*':
+            # Top-left
+            if i > 0 and j > 0:
+                if matrix[i-1][j-1] != '*':
+                    matrix[i-1][j-1] += 1
 
+            # Top
+            if i > 0:
+                if matrix[i-1][j] != '*':
+                    matrix[i-1][j] += 1
+
+            # Top-right
+            if i > 0 and j < n - 1:
+                if matrix[i-1][j+1] != '*':
+                    matrix[i-1][j+1] += 1
+
+            # Left
+            if j > 0:
+                if matrix[i][j-1] != '*':
+                    matrix[i][j-1] += 1
+
+            # Right
+            if j < n - 1:
+                if matrix[i][j+1] != '*':
+                    matrix[i][j+1] += 1
+
+            # Bottom-left
+            if i < m - 1 and j > 0:
+                if matrix[i+1][j-1] != '*':
+                    matrix[i+1][j-1] += 1
+
+            # Bottom
+            if i < m - 1:
+                if matrix[i+1][j] != '*':
+                    matrix[i+1][j] += 1
+
+            # Bottom-right
+            if i < m - 1 and j < n - 1:
+                if matrix[i+1][j+1] != '*':
+                    matrix[i+1][j+1] += 1
 
 board(matrix)
